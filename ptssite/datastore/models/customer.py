41241e647edadc7d6bc4a913361730418313f56c
Soroosh Chabi <chabi@ce.sharif.edu>
2018-01-24 14:21:37 +0330

    Modified the datastore User model and its descendants hierarchy.

--- ptssite/datastore/models/customer.py
+++ ptssite/datastore/models/customer.py
@@ -1,10 +1,6 @@
 from django.db import models
 from . import user
 
-class Customer(user.User):
+class Customer(user.UnprivilegedUser):
     account_balance =  models.BigIntegerField(verbose_name="موجودی",
                                               default=0)
-
-    def __str__(self):
-        fullname = self.first_name + " " + self.last_name
-        return fullname
