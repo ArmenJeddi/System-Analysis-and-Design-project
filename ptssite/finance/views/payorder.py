2ca13a9343c08de1ff2298aded3693a4254bbf59
MHesam <mohammadhesam.berjisian@gmail.com>
2018-01-27 14:31:25 +0330

    updated payorder and depositmoney in finance

--- ptssite/finance/views/payorder.py
+++ ptssite/finance/views/payorder.py
@@ -3,38 +3,40 @@ from django.shortcuts import render
 from datastore.models import Driver, ProductSubmit, Order
 from django import forms
 from django.core.exceptions import ValidationError
+from authentication.decorators import customer_required
 
 template = 'finance/payorder.html'
 
 class PayForm(forms.Form):
-    address = forms.CharField(error_messages={
+    customer_address = forms.CharField(error_messages={
                                     'required': 'لطفا آدرسی را مشخص کنید',
                                 })
-
+@customer_required
 def payorder(request):
     if request.method == 'GET':
-        if 'user_name' in request.session and request.session['role'] == 'customer':
-            submittedProduct = ProductSubmit.objects.get(pk=request.session['name'])
-            assignedDriver = Driver.objects.get(pk=request.session['username'])
-            product_final_price = submittedProduct.quantity * submittedProduct.price
-            driver_name = assignedDriver.first_name + " " + assignedDriver.last_name
-            response = render(request, template, context=dict(form=PayForm(), 
-                                                                submittedProduct=submittedProduct,
-                                                                assignedDriver=assignedDriver,
-                                                                product_final_price=product_final_price,
-                                                                driver_name=driver_name))
-        else:
-            response = render(request, template)
+        submittedProduct = ProductSubmit.objects.get(pk=request.session['name'])
+        assignedDriver = Driver.objects.get(pk=request.session['username'])
+        product_final_price = submittedProduct.quantity * submittedProduct.price
+        driver_name = assignedDriver.first_name + " " + assignedDriver.last_name
+        response = render(request, template, context=dict(form=PayForm(), 
+                                                            submittedProduct=submittedProduct,
+                                                            assignedDriver=assignedDriver,
+                                                            product_final_price=product_final_price,
+                                                            driver_name=driver_name))
     elif request.method == 'POST':
         form = PayForm(request.POST)
         if form.is_valid():
-            order = Order.objects.get(pk=request.session['username'])
-            order.location = form.cleanes_data['address']
-            order.save()
-            response = HttpResponse(status=303)
-            response['Location'] = '/reporting/listpurchases/'
-            #set timer
-            #resign from purchasing
+            if 'pay' in request.POST:
+                order = Order.objects.get(pk=request.user.session['username'])
+                order.location = form.cleanes_data['address']
+                order.save()
+                response = HttpResponse(status=303)
+                response['Location'] = '/reporting/listpurchases/'
+                #set timer
+                #resign from purchasing
+            elif 'cancel' in request.POST:
+                response = HttpResponse(status=303)
+                response['Location'] = '/finance/depositmoney/'
         else:
             response = render(request, template, context=dict(form=form))
     return response
