a2591cdeb1d992fc45ef1f861a759f196d24dbb6
MHesam <mohammadhesam.berjisian@gmail.com>
2018-01-30 13:52:10 +0330

    updated reporting/listtransports

--- ptssite/reporting/views/listdrivers.py
+++ ptssite/reporting/views/listdrivers.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datastore.models import Driver, ProductSubmit, Order
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from authentication.decorators import customer_required
+from authentication.decorators import privileged_required
 
-@customer_required
+@privileged_required
 def listdrivers(request):
     if request.method == 'GET':
         
--- ptssite/reporting/views/listtransports.py
+++ ptssite/reporting/views/listtransports.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datastore.models import order, ProductSubmit, Order
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from authentication.decorators import driver_required
 
+@driver_required
 def listtransports(request):
     if request.method == 'GET':
-        return render(request, 'reporting/listtransports.html')
+        
+        order_list = Order.objects.filter(driver=request.user.username)
+        page = request.GET.get('page')
+
+        paginator = Paginator(order_list, 2)
+        try:
+            orders = paginator.get_page(page)
+        except PageNotAnInteger:
+            orders = paginator.get_page(1)
+        except EmptyPage:
+            orders = paginator.get_page(paginator.num_pages)
+
+        return render(request, 'reporting/listtransports.html', { 'orders': orders })
