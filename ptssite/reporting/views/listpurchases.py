805de217e0c3f48e9f9e4cf4659596a5426229fe
MHesam <mohammadhesam.berjisian@gmail.com>
2018-01-31 15:51:31 +0330

    updated reporting/listpurchases

--- ptssite/reporting/views/listpurchases.py
+++ ptssite/reporting/views/listpurchases.py
@@ -3,8 +3,16 @@ from django.http import HttpResponse
 from datastore.models import Driver, ProductSubmit, Order
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from authentication.decorators import customer_required
+from django import forms
+from django.core.exceptions import ValidationError
+
+template = 'reporting/listpurchases.html'
+
+class ReceptionForm(forms.Form):
+    order = forms.IntegerField()
 
 @customer_required
+    
 def listpurchases(request):
     if request.method == 'GET':
         
@@ -19,4 +27,18 @@ def listpurchases(request):
         except EmptyPage:
             purchases = paginator.get_page(paginator.num_pages)
 
-        return render(request, 'reporting/listpurchases.html', { 'purchases': purchases })
+        response = render(request, 'reporting/listpurchases.html', { 'purchases': purchases })
+        
+    elif request.method == 'POST':
+        
+        form = ReceptionForm(request.POST)
+        if form.is_valid():
+            order = Order.objects.get(pk = form.cleaned_data['order'])
+            order.buyer_receipt = True
+            order.save()
+            response = HttpResponse(status=303)
+            response['Location'] = '/reporting/listpurchases/'
+        else:
+            response = render(request, template, context=dict(form=form))
+    
+    return response
