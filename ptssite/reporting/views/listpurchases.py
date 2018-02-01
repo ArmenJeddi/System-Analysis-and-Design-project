48c70078b9354b5575bfadf88124d1232c94c54b
Ahmadreza_J <ahmadreza.jeddi@gmail.com>
2018-02-01 21:18:39 +0330

    adjusting product purchases by buyer

--- ptssite/reporting/views/listpurchases.py
+++ ptssite/reporting/views/listpurchases.py
@@ -6,6 +6,8 @@ from authentication.decorators import customer_required
 from django import forms
 from django.core.exceptions import ValidationError
 
+from convertdate import persian
+
 template = 'reporting/listpurchases.html'
 
 class ReceptionForm(forms.Form):
@@ -17,10 +19,12 @@ def listpurchases(request):
     if request.method == 'GET':
         
         temp_list = Order.objects.filter(buyer=request.user.username)
-        purchase_list = [[0 for x in range(2)] for y in range(len(temp_list))]
+        purchase_list = [[0 for x in range(3)] for y in range(len(temp_list))]
         for i in range(0, len(temp_list)):
             purchase_list[i][0] = i + 1
             purchase_list[i][1] = temp_list[i]
+            tarikh = persian.from_gregorian(temp_list[i].year, temp_list[i].month, temp_list[i].day)
+            purchase_list[i][2] = tarikh
         page = request.GET.get('page')
 
         paginator = Paginator(purchase_list, 2)
