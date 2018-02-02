0b6726318c7a56826ccafba56f9a4eed89ad14c6
Ahmadreza_J <ahmadreza.jeddi@gmail.com>
2018-02-02 16:28:28 +0330

    css continued

--- ptssite/tradeproduct/urls.py
+++ ptssite/tradeproduct/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.farmerActivity import submitProduct, updateProducts, submit_details, delete_submittedProduct, change_details
+from .views.farmerActivity import submitProduct, submit_details, delete_submittedProduct, change_details
 
 from .views.buyerActivity import browseProduct, selectProduct, selectDriver, driver_details, confirmIt, order_detail_buyer
 
@@ -7,7 +7,6 @@ app_name = 'tradeproduct'
 
 urlpatterns = [
     path('submitProduct/', submitProduct, name = 'submit'),
-    path('updateSubmittedProduct/', updateProducts, name = 'update'),
     path('submit_details/<int:prodsub_id>/', submit_details, name = 'details'),
     path('delete_submittedProduct/<int:delete_id>/', delete_submittedProduct, name = 'delete'),
     path('change_details/<int:change_id>/', change_details, name = 'change'),
